145e7af193fed192341124dc527c0497b18803a9
Peterskill <104547222+Peterskill@users.noreply.github.com>
2023-05-01 23:33:52 +0530

    runtime change

--- main.tf
+++ main.tf
@@ -17,10 +17,12 @@ provider "google" {
 
 resource "google_cloudfunctions_function" "fucntion" {
   name = "vistitor-count-2"
-  runtime = "python310"
+  runtime = "python311"
   available_memory_mb = 256
   source_archive_bucket = "sps-backend"
   source_archive_object = "main.py"
+  max_instances = 1000
+  min_instances = 0
   entry_point = "get_visitor_number"
   trigger_http = true
   region = "us-central1"
