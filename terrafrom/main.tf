042c3a90d2deb468201bb2ef437698ce70344c41
Peterskill <104547222+Peterskill@users.noreply.github.com>
2023-05-21 22:20:08 +0530

    new updated version

--- terrafrom/main.tf
+++ terrafrom/main.tf
@@ -1,14 +1,21 @@
+resource "google_storage_bucket" "Bucket" {
+  name = "sps-backend"
+  location = "US"
+}
 
-
-
+resource "google_storage_bucket_object" "archive" {
+  name = "main.zip"
+  bucket = google_storage_bucket.Bucket.name
+  source = "Functions/main.py"
+}
 
 
 resource "google_cloudfunctions_function" "fucntion" {
   name = "vistitor-count-2"
   runtime = "python310"
   available_memory_mb = 256
-  source_archive_bucket = "sps-backend"
-  source_archive_object = "main.py"
+  source_archive_bucket = google_storage_bucket.Bucket.name
+  source_archive_object = google_storage_bucket_object.archive.name
   max_instances = 1000
   min_instances = 0
   entry_point = "get_visitor_number"
