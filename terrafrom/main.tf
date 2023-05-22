f395b8c4d8b6d3be73f7aaa007e26c96523df919
Peterskill <104547222+Peterskill@users.noreply.github.com>
2023-05-22 18:57:59 +0530

    new addtion

--- terrafrom/main.tf
+++ terrafrom/main.tf
@@ -31,7 +31,8 @@ resource "google_cloudfunctions_function" "fucntion" {
 }
 
 resource "google_cloudfunctions_function_iam_member" "functioniampolicy" {
-  region = "us-central1"
+  project = "sps-cloud-rc"
+  region = google_cloudfunctions_function.fucntion.region
   cloud_function = google_cloudfunctions_function.fucntion.name
   role = "roles/cloudfunctions.invoker"
   member = "allUsers"
